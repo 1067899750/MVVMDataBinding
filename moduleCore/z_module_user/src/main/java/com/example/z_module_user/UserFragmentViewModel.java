87bdac291c4b6d9e559a6438102a0f6e4ca3ad1a
1067899750@qq.com <puyantao123759>
2020-04-25 10:28:16 +0800

    # 添加时间选择器

--- moduleCore/z_module_user/src/main/java/com/example/z_module_user/UserFragmentViewModel.java
+++ moduleCore/z_module_user/src/main/java/com/example/z_module_user/UserFragmentViewModel.java
@@ -1,10 +1,16 @@
 package com.example.z_module_user;
 
 import android.app.Application;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.example.z_lib_base.base.BaseViewModel;
+import com.example.z_lib_base.bus.event.SingleLiveEvent;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.ObservableField;
+
+import java.util.Date;
 
 /**
  * @author puyantao
@@ -12,7 +18,35 @@ import androidx.annotation.NonNull;
  * @create 2020/4/17 17:26
  */
 public class UserFragmentViewModel extends BaseViewModel {
+    public ObservableField<String> dateStr = new ObservableField<>();
+
     public UserFragmentViewModel(@NonNull Application application) {
         super(application);
     }
+
+    public UIChangeObservable uc = new UIChangeObservable();
+    public class UIChangeObservable {
+        public SingleLiveEvent<Object> dateChangeTool = new SingleLiveEvent<>();
+    }
+
+
+    /**
+     *  时间选择器
+     * @param view
+     */
+    public void onDateClick(View view){
+        uc.dateChangeTool.setValue(" ");
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
--- moduleCore/z_module_user/src/main/java/com/example/z_module_user/UserMainFragment.java
+++ moduleCore/z_module_user/src/main/java/com/example/z_module_user/UserMainFragment.java
@@ -1,11 +1,17 @@
 package com.example.z_module_user;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.example.z_lib_base.base.BaseConniveViewModel;
@@ -13,7 +19,11 @@ import com.example.z_lib_base.base.BaseMVVMFragment;
 import com.example.z_lib_base.base.BaseViewModel;
 import com.example.z_lib_common.arouter.ARouterUtils;
 import com.example.z_lib_common.model.NetWorkViewModel;
+import com.example.z_lib_common.utils.ValueUtil;
 import com.example.z_module_user.databinding.UserMainFragmentBinding;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 
 /**
@@ -24,6 +34,7 @@ import com.example.z_module_user.databinding.UserMainFragmentBinding;
  */
 @Route(path = ARouterUtils.USER_MAIN_FRAGMENT)
 public class UserMainFragment extends BaseMVVMFragment<UserMainFragmentBinding, UserFragmentViewModel> {
+    public Context mContext;
 
     public UserMainFragment() {
     }
@@ -33,6 +44,12 @@ public class UserMainFragment extends BaseMVVMFragment<UserMainFragmentBinding, 
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.mContext = context;
     }
 
     @Override
@@ -50,4 +67,89 @@ public class UserMainFragment extends BaseMVVMFragment<UserMainFragmentBinding, 
     public int initVariableId() {
         return BR.viewModel;
     }
+
+
+    @Override
+    public void initData() {
+        super.initData();
+    }
+
+
+    @Override
+    public void initViewObservable() {
+        super.initViewObservable();
+        viewModel.uc.dateChangeTool.observe(this, new Observer<Object>() {
+            @Override
+            public void onChanged(Object o) {
+                if (ValueUtil.isStrEmpty(viewModel.dateStr.get())){
+                    emptyDataChoose();
+                } else {
+                    hasDataChoose();
+                }
+            }
+        });
+    }
+
+
+    private void emptyDataChoose() {
+        Calendar myCalendar = Calendar.getInstance(Locale.CHINA);
+        //获取Calendar对象中的年
+        int year = myCalendar.get(Calendar.YEAR);
+        //获取Calendar对象中的月
+        int month = myCalendar.get(Calendar.MONTH);
+        //获取这个月的第几天
+        int day = myCalendar.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog dpd = new DatePickerDialog(mContext, AlertDialog.THEME_HOLO_LIGHT,
+                new DatePickerDialog.OnDateSetListener() {
+            /**params：view：该事件关联的组件
+             * params：myyear：当前选择的年
+             * params：monthOfYear：当前选择的月
+             * params：dayOfMonth：当前选择的日
+             */
+            @Override
+            public void onDateSet(DatePicker view, int myyear, int monthOfYear, int dayOfMonth) {
+                viewModel.dateStr.set(myyear + "-" + (monthOfYear + 1) + "-" + dayOfMonth);
+            }
+        }, year, month, day);
+        //显示DatePickerDialog组件
+        dpd.show();
+    }
+
+    private void hasDataChoose() {
+        try {
+            String[] dates =  viewModel.dateStr.get().split("[-]");
+            //获取Calendar对象中的年
+            int year = Integer.valueOf(dates[0]);
+            //获取Calendar对象中的月
+            int month = Integer.valueOf(dates[1]) - 1;
+            //获取这个月的第几天
+            int day = Integer.valueOf(dates[2]);
+            DatePickerDialog dpd = new DatePickerDialog(mContext, AlertDialog.THEME_HOLO_LIGHT,
+                    new DatePickerDialog.OnDateSetListener() {
+                /**params：view：该事件关联的组件
+                 * params：myyear：当前选择的年
+                 * params：monthOfYear：当前选择的月
+                 * params：dayOfMonth：当前选择的日
+                 */
+                @Override
+                public void onDateSet(DatePicker view, int myyear, int monthOfYear, int dayOfMonth) {
+                    viewModel.dateStr.set(myyear + "-" + (monthOfYear + 1) + "-" + dayOfMonth);
+                }
+            }, year, month, day);
+            //显示DatePickerDialog组件
+            dpd.show();
+        } catch (Exception e) {
+            emptyDataChoose();
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
